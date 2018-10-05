e389de6ebc3e992381c284c79e3a44fa70fed1eb
Raffaele Mignone <raffaele.mignone@pm.me>
2018-10-05 15:44:25 +0200

    add views and controllers

---
+++ outOfMatrix/outOfMatrix/controllers/ViewControllerMe.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerMe.swift
+//  outOfMatrix
+//
+//  Created by orangeDugongo on 05/10/2018.
+//  Copyright © 2018 TheJagers. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerMe: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
