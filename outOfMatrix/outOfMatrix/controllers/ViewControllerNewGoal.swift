9950df690fe75e369bafb28290681eb04d8e8724
RobertoGarzone <rob.1997.garzone@icloud.com>
2018-10-08 20:54:59 +0200

    Iniziata la compilazione della new goal view

--- outOfMatrix/outOfMatrix/controllers/ViewControllerNewGoal.swift
+++ outOfMatrix/outOfMatrix/controllers/ViewControllerNewGoal.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewControllerNewGoal: UIViewController {
 
+    @IBOutlet weak var rerollLabel: UILabel!
+    @IBOutlet weak var newgoal: UILabel!
+    @IBOutlet weak var monthly: UIButton!
+    @IBOutlet weak var weekly: UIButton!
+    @IBOutlet weak var reroll: UIButton!
+    
+    let roll1 = UIImage(named: "re-roll")
+    
+    var goals = ["learn a new song", "spend time with your family", "have a coffe with a friend", "do 30 miuntes of sport", "run for 500 meters"]
+    
+    var random: Int = 0
+    
+    @IBAction func reroll(_ sender: Any) {
+        random = Int(arc4random()) % goals.count
+        newgoal.text = goals[random]
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        reroll.setBackgroundImage(roll1, for: .normal)
     }
+    
     
 
     /*
---
+++ outOfMatrix/outOfMatrix/controllers/ViewControllerNewMonthlyGoal.swift
@@ -0,0 +1,38 @@
+//
+//  ViewControllerNewMonthlyGoal.swift
+//  outOfMatrix
+//
+//  Created by garzoneroberto on 08/10/18.
+//  Copyright © 2018 TheJagers. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerNewMonthlyGoal: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+/*    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "segueToWeekly") {
+            let vc = segue.destination as! ViewControllerNewWeeklyGoal
+        }
+        else if (segue.identifier == "segueToMpnthly") {
+            let vc = segue.destination as! ViewControllerNewMonthlyGoal
+        }
+    } */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
